864e211b217a3ef6fd74542fb878bc0ce95e4884
sathish0706 <107674282+sathish0706@users.noreply.github.com>
2022-08-04 01:34:25 -0600

    Add files via upload

---
+++ callback hell/script.js
@@ -0,0 +1,64 @@
+var t=10;
+ setTimeout(()=>{
+  document.body.append(document.createElement("h4").innerHTML=t--)
+  document.body.append(document.createElement("br"))
+  document.body.append(document.createElement("br"))
+},1000);
+
+setTimeout(()=>{
+  document.body.append(document.createElement("h4").innerHTML=t--)
+  document.body.append(document.createElement("br"))
+  document.body.append(document.createElement("br"))
+},2000);
+
+ setTimeout(()=>{
+  document.body.append(document.createElement("h4").innerHTML=t--)
+  document.body.append(document.createElement("br"))
+  document.body.append(document.createElement("br"))
+},3000);
+
+setTimeout(()=>{
+  document.body.append(document.createElement("h4").innerHTML=t--)
+  document.body.append(document.createElement("br"))
+  document.body.append(document.createElement("br"))
+},4000);
+setTimeout(()=>{
+  document.body.append(document.createElement("h4").innerHTML=t--)
+  document.body.append(document.createElement("br"))
+  document.body.append(document.createElement("br"))
+},5000)
+setTimeout(()=>{
+  document.body.append(document.createElement("h4").innerHTML=t--)
+  document.body.append(document.createElement("br"))
+  document.body.append(document.createElement("br"))
+},6000)
+setTimeout(()=>{
+  document.body.append(document.createElement("h4").innerHTML=t--)
+  document.body.append(document.createElement("br"))
+  document.body.append(document.createElement("br"))
+},7000)
+setTimeout(()=>{
+  document.body.append(document.createElement("h4").innerHTML=t--)
+  document.body.append(document.createElement("br"))
+  document.body.append(document.createElement("br"))
+},8000)
+setTimeout(()=>{
+  document.body.append(document.createElement("h4").innerHTML=t--)
+  document.body.append(document.createElement("br"))
+  document.body.append(document.createElement("br"))
+},9000)
+setTimeout(()=>{
+  document.body.append(document.createElement("h4").innerHTML=t--)
+  document.body.append(document.createElement("br"))
+  document.body.append(document.createElement("br"))
+  document.body.append(document.createElement("br"))
+},10000)
+
+
+
+setTimeout(()=> {
+ var h1=document.createElement("h1");
+ h1.innerHTML = "Happy Independence Day..!";
+ document.body.append(h1)
+ return h1
+},11000)
