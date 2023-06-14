bfda6a032a7584495b45705f222ddd45c03e0957
Sathish R <107674282+sathish0706@users.noreply.github.com>
2023-06-14 10:47:22 +0100

    Update script.js

--- callback hell/script.js
+++ callback hell/script.js
@@ -1,64 +1,37 @@
-var t=10;
- setTimeout(()=>{
-  document.body.append(document.createElement("h4").innerHTML=t--)
-  document.body.append(document.createElement("br"))
-  document.body.append(document.createElement("br"))
-},1000);
+var time = document.createElement("h1");
+document.body.append(time);
+var num = 10;
 
-setTimeout(()=>{
-  document.body.append(document.createElement("h4").innerHTML=t--)
-  document.body.append(document.createElement("br"))
-  document.body.append(document.createElement("br"))
-},2000);
-
- setTimeout(()=>{
-  document.body.append(document.createElement("h4").innerHTML=t--)
-  document.body.append(document.createElement("br"))
-  document.body.append(document.createElement("br"))
-},3000);
-
-setTimeout(()=>{
-  document.body.append(document.createElement("h4").innerHTML=t--)
-  document.body.append(document.createElement("br"))
-  document.body.append(document.createElement("br"))
-},4000);
-setTimeout(()=>{
-  document.body.append(document.createElement("h4").innerHTML=t--)
-  document.body.append(document.createElement("br"))
-  document.body.append(document.createElement("br"))
-},5000)
-setTimeout(()=>{
-  document.body.append(document.createElement("h4").innerHTML=t--)
-  document.body.append(document.createElement("br"))
-  document.body.append(document.createElement("br"))
-},6000)
-setTimeout(()=>{
-  document.body.append(document.createElement("h4").innerHTML=t--)
-  document.body.append(document.createElement("br"))
-  document.body.append(document.createElement("br"))
-},7000)
-setTimeout(()=>{
-  document.body.append(document.createElement("h4").innerHTML=t--)
-  document.body.append(document.createElement("br"))
-  document.body.append(document.createElement("br"))
-},8000)
-setTimeout(()=>{
-  document.body.append(document.createElement("h4").innerHTML=t--)
-  document.body.append(document.createElement("br"))
-  document.body.append(document.createElement("br"))
-},9000)
-setTimeout(()=>{
-  document.body.append(document.createElement("h4").innerHTML=t--)
-  document.body.append(document.createElement("br"))
-  document.body.append(document.createElement("br"))
-  document.body.append(document.createElement("br"))
-},10000)
-
-
-
-setTimeout(()=> {
- var h1=document.createElement("h1");
- h1.innerHTML = "Happy Independence Day..!";
- document.body.append(h1)
- return h1
-},11000)
+setTimeout(() => {
+  time.innerHTML = num;
+  setTimeout(() => {
+    time.innerHTML = --num;
+    setTimeout(() => {
+      time.innerHTML = --num;
+      setTimeout(() => {
+        time.innerHTML = --num;
+        setTimeout(() => {
+          time.innerHTML = --num;
+          setTimeout(() => {
+            time.innerHTML = --num;
+            setTimeout(() => {
+              time.innerHTML = --num;
+              setTimeout(() => {
+                time.innerHTML = --num;
+                setTimeout(() => {
+                  time.innerHTML = --num;
+                  setTimeout(() => {
+                    time.innerHTML = --num;
+                    setTimeout(() => {
+                      time.innerHTML = "Happy indipendence day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
